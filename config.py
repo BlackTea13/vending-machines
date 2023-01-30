8846832fcb1344a0799eef999378bb58b37f213d
BlackTea13 <robertblum2001@gmail.com>
2023-01-30 22:50:08 +0700

    [unittesting] added docker compose file, changed config file accordingly

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import yaml
 
 
 class Config:
-    MYSQL_HOST = '127.0.0.1'
+    MYSQL_HOST = '127.0.0.1:13306'
     MYSQL_USERNAME = 'root'
-    MYSQL_PASSWORD = 'iccsroot'
+    MYSQL_PASSWORD = 'vending-pw'
     MYSQL_DATABASE = 'vending_machines'
 
     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{MYSQL_USERNAME}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DATABASE}'
